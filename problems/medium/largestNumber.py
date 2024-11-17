1163afd783ebb404e83ff56d60d27b55725ee6a8
amy z <37263746+0melette@users.noreply.github.com>
2024-11-17 12:20:34 +1100

    Fix largestNumber and add pre-push hook

--- problems/medium/largestNumber.py
+++ problems/medium/largestNumber.py
@@ -3,7 +3,7 @@ from typing import List
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
             nums = list(map(str, nums))
-            nums.sort(key=lambda x: x*2, reverse=True)
+            nums.sort(key=lambda x: x*10, reverse=True)
             
             if nums[0] == '0':
                 return '0'
