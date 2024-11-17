cb4e355315022f1bb270c69fa69ef686c784951c
amy z <37263746+0melette@users.noreply.github.com>
2024-11-17 12:13:50 +1100

    Fix UncommonFromSentences Implementation

--- problems/easy/uncommonFromSentences.py
+++ problems/easy/uncommonFromSentences.py
@@ -1,6 +1,15 @@
+# https://leetcode.com/problems/uncommon-words-from-two-sentences/description/
 from typing import List
 
 class Solution:
     def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
-        s1.split()
-        print(s1)
+        words = s1.split() + s2.split()
+        word_count = {}
+        for word in words:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+            
+        uncommon_words = [word for word, count in word_count.items() if count == 1]
+        
+        return uncommon_words
